53c0a941e2bf543cef3fb2001af903d2dc546b54
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2023-02-06 10:16:35 +0900

    ♻️ queryをexecuteに書き換え

--- app/crud.py
+++ app/crud.py
@@ -8,19 +8,19 @@ import performance
 
 
 def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+    return db.execute(sqlalchemy.select(models.User).filter(models.User.id == user_id)).scalars().first()
 
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
+    return db.execute(sqlalchemy.select(models.User).filter(models.User.email == email)).scalars().first()
 
 
 def get_user_by_screen_name(db: Session, screen_name: str):
-    return db.query(models.User).filter(models.User.screen_name == screen_name).first()
+    return db.execute(sqlalchemy.select(models.User).filter(models.User.screen_name == screen_name)).scalars().first()
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
+    return db.execute(sqlalchemy.select(models.User).offset(skip).limit(limit)).scalars().all()
 
 
 def create_user(db: Session, user: schemas.UserCreate, hashed_password: str):
@@ -33,27 +33,38 @@ def create_user(db: Session, user: schemas.UserCreate, hashed_password: str):
 
 def get_score(db: Session, folder: str, filename: str, player_id: int):
     return (
-        db.query(models.Score)
-        .filter(models.Score.folder == folder)
-        .filter(models.Score.filename == filename)
-        .filter(models.Score.player_id == player_id)
+        db.execute(
+            sqlalchemy.select(models.Score)
+            .filter(models.Score.folder == folder)
+            .filter(models.Score.filename == filename)
+            .filter(models.Score.player_id == player_id)
+        )
+        .scalars()
         .first()
     )
 
 
 def get_scores(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Score).offset(skip).limit(limit).all()
+    return db.execute(sqlalchemy.select(models.Score).offset(skip).limit(limit)).scalars().all()
 
 
 def get_scores_by_beatmap(db: Session, folder: str, filename: str):
-    return db.query(models.Score).filter(models.Score.folder == folder).filter(models.Score.filename == filename).all()
+    return (
+        db.execute(
+            sqlalchemy.select(models.Score)
+            .filter(models.Score.folder == folder)
+            .filter(models.Score.filename == filename)
+        )
+        .scalars()
+        .all()
+    )
 
 
 def create_score(db: Session, score: schemas.ScoreCreate, player_id: int):
     db_score = models.Score(
         **score.dict(),
         player_id=player_id,
-        submitted_on=datetime.now(timezone(timedelta(hours=0)))+timedelta(hours=9),
+        submitted_on=datetime.now(timezone(timedelta(hours=0))) + timedelta(hours=9),
         performance_point=performance.calculate(score=score.score, level=score.level)
     )
     db.add(db_score)
@@ -63,15 +74,8 @@ def create_score(db: Session, score: schemas.ScoreCreate, player_id: int):
 
 
 def update_score(db: Session, score: schemas.ScoreCreate, player_id: int):
-    db_score = (
-        db.query(models.Score)
-        .filter(models.Score.folder == score.folder)
-        .filter(models.Score.filename == score.filename)
-        .filter(models.Score.player_id == player_id)
-        .first()
-    )
-    assert db_score is not None
-    db_score.submitted_on = datetime.now(timezone(timedelta(hours=0)))+timedelta(hours=9)
+    db_score: schemas.Score = get_score(db, folder=score.folder, filename=score.filename, player_id=player_id)
+    db_score.submitted_on = datetime.now(timezone(timedelta(hours=0))) + timedelta(hours=9)
     db_score.performance_point = performance.calculate(score=score.score, level=score.level)
 
     for key, value in score.dict().items():
@@ -83,7 +87,7 @@ def update_score(db: Session, score: schemas.ScoreCreate, player_id: int):
 
 
 def update_ranking(db: Session):
-    db_users = get_users(db)
+    db_users: list[schemas.UserScore] = get_users(db)
     for user in db_users:
         performance_point = 0
         for i, score in enumerate(sorted(user.scores, key=lambda score: score.performance_point, reverse=True)[:30]):
