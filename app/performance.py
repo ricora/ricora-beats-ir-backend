cc803a86b419df70976d945afb8ebfb3a64f080c
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2023-02-06 09:21:21 +0900

    👕 パフォーマンス計算用関数の返り値のtype hintを追加

--- app/performance.py
+++ app/performance.py
@@ -1,6 +1,6 @@
 import math
 
 
-def calculate(score: float, level: int):
+def calculate(score: float, level: int) -> int:
     ratio = 0.8 / (1 + math.exp(-0.8 * score + 74)) if score <= 95.371 else 0.059 * (score - 100) + 1
-    return ratio * level * 50
+    return int(ratio * level * 50)
