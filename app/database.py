5137bbc15bbe8a61f7f3cd4eda9a68d3dd7f1793
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2023-11-11 07:06:56 +0900

    ✨ SQL実行前のコネクションの確認と再接続を有効にするために`pool_pre_ping=True`を追加 (#1)

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ import os
 
 SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"].replace("postgres://", "postgresql://")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
